34a6eb1304de5a45974814038133a4204868ded8
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-08-06 14:06:29 -0700

    Add generate random number of customers function

--- js/app.js
+++ js/app.js
@@ -6,28 +6,40 @@ const $1standPike = {
   minCustomers: 23,
   maxCustomers: 65,
   avgCookies: 6.3,
+  generateCustomers: generateRandomCustomers,
 };
 
 const seaTacAirport = {
   minCustomers: 3,
   maxCustomers: 24,
   avgCookies: 1.2,
+  generateCustomers: generateRandomCustomers,
 };
 
 const seattleCenter = {
   minCustomers: 11,
   maxCustomers: 38,
   avgCookies: 3.7,
+  generateCustomers: generateRandomCustomers,
 };
 
 const capitolHill = {
   minCustomers: 20,
   maxCustomers: 38,
   avgCookies: 2.3,
+  generateCustomers: generateRandomCustomers,
 };
 
 const alki = {
   minCustomers: 2,
   maxCustomers: 16,
   avgCookies: 4.6,
+  generateCustomers: generateRandomCustomers,
 };
+
+function generateRandomCustomers() {
+  let min = Math.ceil(this.minCustomers);
+  let max = Math.floor(this.maxCustomers);
+
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
