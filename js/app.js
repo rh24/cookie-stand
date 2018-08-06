5736bab40b83ec7118fcd2f5a60a3a585f09bf26
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-08-06 13:30:04 -0700

    Add salmon picture to body background

--- js/app.js
+++ js/app.js
@@ -1,3 +1,33 @@
 'use strict';
 
 console.log('the js is linked');
+
+const $1standPike = {
+  minCustomers: 23,
+  maxCustomers: 65,
+  avgCookies: 6.3,
+};
+
+const seaTacAirport = {
+  minCustomers: 3,
+  maxCustomers: 24,
+  avgCookies: 1.2,
+};
+
+const seattleCenter = {
+  minCustomers: 11,
+  maxCustomers: 38,
+  avgCookies: 3.7,
+};
+
+const capitolHill = {
+  minCustomers: 20,
+  maxCustomers: 38,
+  avgCookies: 2.3,
+};
+
+const alki = {
+  minCustomers: 2,
+  maxCustomers: 16,
+  avgCookies: 4.6,
+};
