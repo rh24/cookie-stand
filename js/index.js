2f6a65ce9f318b79ebc6312a35c2f12c05bbf25d
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-08-09 14:45:10 -0700

    Add js file to handle index click events

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function attachEventListeners() {
+  // who we are
+  // our source
+  // our cookies
+  // merchandise
+  // our locations
+  // contact us
+
+}
+
+function appendTextToParagraph() {
+  let paragraph = document.getElementsByTagName('article')[0].getElementsByTagName('p')[0];
+}
