c6d97d6a558f64ae08ad462abda1d50049abf4ba
Rebecca Hong <ms.rebeccahong@gmail.com>
2018-08-09 16:28:14 -0700

    Append text onto page

--- js/index.js
+++ js/index.js
@@ -5,7 +5,9 @@ const pageData = [
       './assets/family.jpg'
     ],
     header: '<h2>About Us</h2>',
-    text: 'Hi, we\'re the Smith family. We\'ve been making our salmon cookies recipe for 89 years. It\'s been passed down for three generations, and we don\'t plan to stop creating these fish cookies for our customers any time soon. <br /><br /> My wife, Caitlin, has been my partner in our business for fifteen years. My two children are our taste testers. They\'re also our biggest fans. <br /><br /> Come visit any of our locations to taste these treats for yourselves. They\'re seriously addictive!'
+    text: [
+      '<h2>About Us</h2> Hi, we\'re the Smith family. We\'ve been making our salmon cookies for 89 years. It\'s been passed down for three generations, and we don\'t plan on stopping the production of these delightful morsels any time soon. Our customers are our treasure as is our secret recipe. <br /><br /> My wife, Caitlin, has been my partner in our business for fifteen years. My two children are our taste testers. They\'re also our biggest fans. <br /><br /> Come visit any of our locations to taste these treats for yourselves. They\'re seriously addictive!'
+    ]
   },
   {
     id: 'source',
@@ -14,7 +16,7 @@ const pageData = [
       './assets/chinook.jpg'
     ],
     text: [
-      'Lorem ipsum dolor amet godard next level readymade seitan ramps everyday carry brooklyn occupy. VHS crucifix yuccie snackwave. Next level kale chips PBR&B normcore swag. Cliche neutra raclette cardigan, chartreuse tote bag bicycle rights plaid yr biodiesel keffiyeh. Stumptown normcore microdosing tumeric hella farm-to-table hot chicken fashion axe pickled snackwave authentic banjo retro keffiyeh williamsburg. Drinking vinegar portland four loko vinyl fixie tbh lumbersexual tousled. Slow-carb vegan brunch synth tousled farm-to-table mlkshk skateboard before they sold out humblebrag. <br />Hammock bushwick letterpress swag YOLO wolf drinking vinegar literally. Hashtag austin XOXO bicycle rights taiyaki fanny pack. Swag ethical four loko hot chicken, normcore literally whatever forage tilde paleo microdosing. Tilde four loko fashion axe pok pok crucifix tbh fam kitsch raw denim beard tote bag marfa.',
+      '<h2>Fresh, Wild Caught</h2> Lorem ipsum dolor amet godard next level readymade seitan ramps everyday carry brooklyn occupy. VHS crucifix yuccie snackwave. Next level kale chips PBR&B normcore swag. Cliche neutra raclette cardigan, chartreuse tote bag bicycle rights plaid yr biodiesel keffiyeh. Stumptown normcore microdosing tumeric hella farm-to-table hot chicken fashion axe pickled snackwave authentic banjo retro keffiyeh williamsburg. Drinking vinegar portland four loko vinyl fixie tbh lumbersexual tousled. Slow-carb vegan brunch synth tousled farm-to-table mlkshk skateboard before they sold out humblebrag. <br />Hammock bushwick letterpress swag YOLO wolf drinking vinegar literally. Hashtag austin XOXO bicycle rights taiyaki fanny pack. Swag ethical four loko hot chicken, normcore literally whatever forage tilde paleo microdosing. Tilde four loko fashion axe pok pok crucifix tbh fam kitsch raw denim beard tote bag marfa.',
       'Umami tacos leggings meh cred, vice kale chips paleo mixtape blue bottle forage kombucha semiotics drinking vinegar blog. Tilde knausgaard enamel pin mumblecore, fashion axe waistcoat cloud bread plaid mixtape disrupt. Kinfolk try-hard gastropub roof party mlkshk celiac, godard taiyaki ennui shaman pug. Ennui pinterest portland brunch, lumbersexual bespoke ethical drinking vinegar polaroid.'
     ]
   },
@@ -26,7 +28,7 @@ const pageData = [
       './assets/frosted-cookie.jpg'
     ],
     text: [
-      'Cold-pressed XOXO skateboard four dollar toast brooklyn tousled roof party, air plant shaman live-edge pop-up vexillologist lyft bicycle rights. Sustainable butcher shaman, raw denim la croix banh mi lyft try-hard tofu readymade. Kinfolk shaman kickstarter farm-to-table vape coloring book knausgaard, food truck blue bottle mustache. Food truck cold-pressed affogato, cred mlkshk XOXO normcore post-ironic chia fanny pack. Franzen typewriter vinyl vexillologist pabst edison bulb, lomo iceland normcore pug portland pinterest taiyaki cred bicycle rights. Tumeric celiac viral fixie kinfolk green juice.',
+      '<h2>In the Kitchen</h2>Cold-pressed XOXO skateboard four dollar toast brooklyn tousled roof party, air plant shaman live-edge pop-up vexillologist lyft bicycle rights. Sustainable butcher shaman, raw denim la croix banh mi lyft try-hard tofu readymade. Kinfolk shaman kickstarter farm-to-table vape coloring book knausgaard, food truck blue bottle mustache. Food truck cold-pressed affogato, cred mlkshk XOXO normcore post-ironic chia fanny pack. Franzen typewriter vinyl vexillologist pabst edison bulb, lomo iceland normcore pug portland pinterest taiyaki cred bicycle rights. Tumeric celiac viral fixie kinfolk green juice.',
       'Kickstarter YOLO sartorial actually ugh, unicorn venmo man bun roof party brooklyn lomo etsy. Chambray iPhone authentic man bun locavore. Gluten-free cold-pressed readymade vice meditation. Cardigan green juice squid ennui.',
     ]
   },
@@ -115,10 +117,17 @@ function appendImage(id) {
     }
   }
 
+  appendTextToParagraph(id);
 }
 
-function appendTextToParagraph() {
+function appendTextToParagraph(id) {
   let paragraph = document.getElementsByTagName('article')[0].getElementsByTagName('p')[0];
+
+  for (let obj of pageData) {
+    if (obj.id === id) {
+      paragraph.innerHTML = obj.text[0];
+    }
+  }
 }
 
 attachEventListeners();
